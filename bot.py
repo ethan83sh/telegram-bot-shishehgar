56780a0776edb17559ca46ab6f2369471b09616d
ethan83sh <132137582+ethan83sh@users.noreply.github.com>
2026-01-22 14:41:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,22 @@
 import os
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from handlers.menu import main_menu
-from handlers.post_creator import start_new_post, handle_post_flow
-from handlers.auto_poster import start_auto
-from handlers.stats import channel_stats
+from handlers.live_post import start_live_post, handle_live_flow
 
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 app = Application.builder().token(TOKEN).build()
 
+# منوی اصلی
 async def start(update, context):
     if update.effective_user.id != ADMIN_ID:
         return
-    await update.message.reply_text("کنترل پنل مدیریت محتوا", reply_markup=main_menu())
+    await update.message.reply_text("پنل مدیریت محتوا", reply_markup=main_menu())
 
+# هاندلرها
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CallbackQueryHandler(start_new_post, pattern="new_post"))
-app.add_handler(CallbackQueryHandler(channel_stats, pattern="stats"))
-app.add_handler(MessageHandler(filters.ALL, handle_post_flow))
+app.add_handler(CallbackQueryHandler(start_live_post, pattern="live_post"))
+app.add_handler(MessageHandler(filters.ALL, handle_live_flow))
 
-start_auto(app)
 app.run_polling()
