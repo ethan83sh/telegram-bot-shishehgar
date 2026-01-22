9f98a830793666741554860a98f0866303c5f03c
ethan83sh <132137582+ethan83sh@users.noreply.github.com>
2026-01-22 15:06:57 +0100

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -4,7 +4,7 @@ def main_menu():
     keyboard = [
         [InlineKeyboardButton("📤 ارسال پست", callback_data="new_post")],
         [InlineKeyboardButton("⏱ پست خودکار", callback_data="auto_post")],
-        [InlineKeyboardButton("📊 آمار", callback_data="stats")]
+        [InlineKeyboardButton("📊 آمار", callback_data="stats")],
         [InlineKeyboardButton("📤 پست لایو جدید", callback_data="live_post")],
     ]
     return InlineKeyboardMarkup(keyboard)
