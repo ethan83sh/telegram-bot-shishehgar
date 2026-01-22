8b6c919f2c2382d1e3d6be1c785a0ef51d831600
ethan83sh <132137582+ethan83sh@users.noreply.github.com>
2026-01-22 14:24:44 +0100

    Update auto_poster.py

--- handlers/auto_poster.py
+++ handlers/auto_poster.py
@@ -1,15 +1,26 @@
 import os
+from telegram import InputFile
 
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 AUTO_INTERVAL = int(os.getenv("AUTO_INTERVAL"))
 AUTO_MESSAGE = os.getenv("AUTO_MESSAGE")
 HASHTAGS = os.getenv("DEFAULT_HASHTAGS")
+AUTO_IMAGE = os.getenv("AUTO_IMAGE")  # لینک یا path
 
 async def auto_post_job(context):
-    await context.bot.send_message(
-        CHANNEL_ID,
-        AUTO_MESSAGE + HASHTAGS
-    )
+    final_text = AUTO_MESSAGE + HASHTAGS
+
+    if AUTO_IMAGE:  # اگر عکس تنظیم شده
+        await context.bot.send_photo(
+            chat_id=CHANNEL_ID,
+            photo=AUTO_IMAGE,
+            caption=final_text
+        )
+    else:  # فقط متن
+        await context.bot.send_message(
+            chat_id=CHANNEL_ID,
+            text=final_text
+        )
 
 def start_auto(app):
     app.job_queue.run_repeating(auto_post_job, interval=AUTO_INTERVAL, first=30)
