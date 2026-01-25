4686b6247af30118828f070e0d88ec2a83bd96d7
ethan83sh <132137582+ethan83sh@users.noreply.github.com>
2026-01-25 13:05:41 +0100

    Update stats.py

--- handlers/stats.py
+++ handlers/stats.py
@@ -7,7 +7,7 @@ from telegram.ext import ContextTypes
 
 # ================= CONFIG TELETHON =================
 API_ID = int(os.getenv("TG_API_ID"))
-API_HASH = os.getenv("TG_API_HASH"))
+API_HASH = os.getenv("TG_API_HASH")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))  # عدد ID کانال
 SESSION_FILE = "bot_session.session"       # مسیر فایل session ساخته شده
 
