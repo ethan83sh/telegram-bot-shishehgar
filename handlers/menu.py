4c1ae565f6c22cd16930a8e9a859b84edf145e2d
ethan83sh <132137582+ethan83sh@users.noreply.github.com>
2026-01-30 13:50:08 +0100

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -3,10 +3,10 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 def main_menu():
     keyboard = [
-        [InlineKeyboardButton("✏️ پست دستی", callback_data="manual_post")],
+        [InlineKeyboardButton("📝 پست دستی", callback_data="new_post")],
         [InlineKeyboardButton("🤖 پست خودکار", callback_data="auto_post")],
-        [InlineKeyboardButton("🎬 لایو", callback_data="live_post")],
-        [InlineKeyboardButton("⏰ تایم زون", callback_data="timezone")],
-        [InlineKeyboardButton("🖊️ مشاهده / تغییر امضا", callback_data="signature")],
+        [InlineKeyboardButton("🎬 پست لایو", callback_data="live_post")],
+        [InlineKeyboardButton("🌐 تایم‌زون", callback_data="timezone")],
+        [InlineKeyboardButton("📅 لایوهای برنامه‌ریزی شده", callback_data="scheduled_lives")]
     ]
     return InlineKeyboardMarkup(keyboard)
